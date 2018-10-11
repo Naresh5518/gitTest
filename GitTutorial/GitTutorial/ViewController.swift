95cff12b1b3378ac49ee0c75267b82d03f4bdc6a
Naresh5518 <44048522+Naresh5518@users.noreply.github.com>
2018-10-11 11:31:55 +0530

    Add files via upload

---
+++ GitTutorial/GitTutorial/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by mTL NARESH on 10/10/18.
+//  Copyright © 2018 mTL NARESH. All rights reserved.
+//
+
+import UIKit
+let message = "Naresh";
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(message)
+        print(message)
+    }
+
+
+}
+
